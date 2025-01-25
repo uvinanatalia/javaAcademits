6f01add4662642d200b7ebca029c3fd836d51043
Uvina Natalia <uvinanatalia@gmail.com>
2025-01-25 21:25:39 +0700

    Initial commit

---
+++ src/academits/java/cyclesFor.java
@@ -0,0 +1,13 @@
+package academits.java;
+
+public class cyclesFor {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 1; i = i - 4) {
+            if (i % 4 == 0) {
+                    System.out.println(i + "");
+
+            }
+        }
+    }
+}
+
