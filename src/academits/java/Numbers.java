33e9a4c0e39d4536094fbbff8c5bdf8a8edfc8e3
Uvina Natalia <uvinanatalia@gmail.com>
2025-01-26 00:51:16 +0700

    add

--- src/academits/java/Numbers.java
+++ src/academits/java/Numbers.java
@@ -6,7 +6,9 @@ public class Numbers {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        //Найдите сумму цифр
+
+        /*Scanner scanner = new Scanner(System.in);
         System.out.println("Введите целое число: ");
         int i = scanner.nextInt();
         int sum = 0;
@@ -17,7 +19,35 @@ public class Numbers {
 
         }
 
-        System.out.println("Сумма цифр числа: " + sum);
+        System.out.println("Сумма цифр числа: " + sum);*/
 
+        /* Найдите сумму только тех цифр числа, которые являются
+нечетными числами*/
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число: ");
+        int n = scanner.nextInt();
+        int sum = 0;
+        while (n > 0) {
+            int lastNumber = n % 10; // последняя цифра из числа
+            if (lastNumber % 2 != 0) {
+                sum = sum + lastNumber;
+            }
+            n = n / 10; // удаление остатка, получаем целое число
+        }
+            System.out.println("Сумма нечетных цифр числа: " + sum);*/
+
+        //Найдите максимальную цифру числа
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число: ");
+        int n = scanner.nextInt();
+        int maxNumber = 0;
+        while (n > 0) {
+            int lastNumber = n % 10; // последняя цифра из числа
+            if (lastNumber > maxNumber) {
+                maxNumber = lastNumber;
+            }
+                n = n / 10; // удаление остатка, получаем целое число
+            }
+        System.out.println("Максимальное число: " + maxNumber);
     }
 }
